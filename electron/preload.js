d0e4c23a6480e78061e18a5c0c9e35845699b9f9
Phiraphat <peerapatmek123@gmail.com>
2026-07-02 11:49:04 +0700

    Add getStatus method to electronAPI

--- electron/preload.js
+++ electron/preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld("electronAPI", {
   listPorts: () => ipcRenderer.invoke("serial:list"),
   connectPort: (portName) => ipcRenderer.invoke("serial:connect", portName),
   disconnectPort: () => ipcRenderer.invoke("serial:disconnect"),
+  getStatus: () => ipcRenderer.invoke("serial:status"),
   sendJointAngles: (data) => ipcRenderer.invoke("serial:send", data),
 });
