48b8cd9dcf8052556b4524d76ba748207d9a3b6b
Phiraphat <peerapatmek123@gmail.com>
2026-07-20 19:32:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+
   plugins: [react()],
-  base: "./", // สำคัญ: ให้ path ไฟล์ทำงานถูกต้องเมื่อรันใน Electron (file://)
+
+  base: "./",
+
+  assetsInclude: ["**/*.glb"],
+
   build: {
     outDir: "dist",
   },
+
 });
